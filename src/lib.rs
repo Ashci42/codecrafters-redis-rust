1c5f9503dfb36adccd4ee113aa6e1a06c9a16f1c
Alex Olteanu <alexandruolteanu42@gmail.com>
2024-11-01 23:26:42 +0000

    Respond to multiple PINGs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
-use std::{io::Write, net::TcpStream};
+use std::io::{BufRead, Write};
 
-pub fn handle_connection(mut stream: TcpStream) -> anyhow::Result<()> {
-    stream.write_all(b"+PONG\r\n")?;
+pub fn handle_connection(mut stream: std::net::TcpStream) -> anyhow::Result<()> {
+    let read_stream = stream.try_clone()?;
+    let buf_reader = std::io::BufReader::new(&read_stream);
+    for command in buf_reader.lines() {
+        if command.is_ok() && command.unwrap() == "PING" {
+            stream.write_all(b"+PONG\r\n")?;
+        }
+    }
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use std::net::TcpListener;
-
 use redis_starter_rust::handle_connection;
 
 fn main() -> anyhow::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:6379")?;
+    let listener = std::net::TcpListener::bind("127.0.0.1:6379")?;
 
     for stream in listener.incoming() {
         let stream = stream?;
