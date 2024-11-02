09f2efa63e8b5d7570f0cc1e1c2b24e1fa225c9a
Alex Olteanu <alexandruolteanu42@gmail.com>
2024-11-02 09:44:18 +0000

    Handle concurrent clients

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,35 @@
-use std::io::{BufRead, Write};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt};
 
-pub fn handle_connection(mut stream: std::net::TcpStream) -> anyhow::Result<()> {
-    let read_stream = stream.try_clone()?;
-    let buf_reader = std::io::BufReader::new(&read_stream);
-    for command in buf_reader.lines() {
-        if command.is_ok() && command.unwrap() == "PING" {
-            stream.write_all(b"+PONG\r\n")?;
-        }
+pub async fn run(addr: &str) -> anyhow::Result<()> {
+    let listener = tokio::net::TcpListener::bind(addr).await?;
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            handle_connection(stream).await?;
+
+            Ok::<(), anyhow::Error>(())
+        });
+    }
+}
+
+async fn handle_connection(mut stream: tokio::net::TcpStream) -> anyhow::Result<()> {
+    let (read_half, mut write_half) = stream.split();
+    let buf_reader = tokio::io::BufReader::new(read_half);
+    let mut lines = buf_reader.lines();
+    while let Some(command) = lines.next_line().await? {
+        handle_command(&mut write_half, &command).await?;
+    }
+
+    Ok(())
+}
+
+async fn handle_command<W>(writer: &mut W, command: &str) -> anyhow::Result<()>
+where
+    W: AsyncWriteExt + Unpin,
+{
+    if command == "PING" {
+        writer.write_all(b"+PONG\r\n").await?;
     }
 
     Ok(())
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,4 @@
-use redis_starter_rust::handle_connection;
-
-fn main() -> anyhow::Result<()> {
-    let listener = std::net::TcpListener::bind("127.0.0.1:6379")?;
-
-    for stream in listener.incoming() {
-        let stream = stream?;
-        handle_connection(stream)?;
-    }
-
-    Ok(())
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    redis_starter_rust::run("127.0.0.1:6379").await
 }
